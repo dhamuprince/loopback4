abfb490726b0c89225289436dc243b504280bf00
dhamuprince <dhamupmtech@gmail.com>
2021-05-12 16:32:59 +0530

    Add model relations using hasOne@

--- src/models/todo-list.model.ts
+++ src/models/todo-list.model.ts
@@ -1,7 +1,8 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, model, property, hasMany, hasOne} from '@loopback/repository';
 // import {Todo} from './todo.model';
 // add TodoWithRelations to the following import
 import {Todo, TodoWithRelations} from './todo.model';
+import {TodoListImage,TodoListImageWithRelations} from './todo-list-image.model';
 
 @model()
 export class TodoList extends Entity {
@@ -26,6 +27,9 @@ export class TodoList extends Entity {
   @hasMany(() => Todo)
   todosfirst: Todo[];
 
+  @hasOne(() => TodoListImage)
+  image: TodoListImage;
+
   constructor(data?: Partial<TodoList>) {
     super(data);
   }
@@ -36,6 +40,8 @@ export interface TodoListRelations {
 
   // add the following line
   todos?: TodoWithRelations[];
+  // Add the following line
+  image?: TodoListImageWithRelations;
 }
 
 export type TodoListWithRelations = TodoList & TodoListRelations;
